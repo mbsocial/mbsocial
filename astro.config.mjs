87518f70dca81dfc2085c169fa53b441a7159151
mbsocial <mbsocial.site@outlook.com>
2025-04-30 22:30:13 -0300

    Update image paths and navigation links to reflect new base URL structure for MBSocial

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://mbsocial.github.io',
-	base: '/mbsocial/',
+	base: '/mbsocial',
 	integrations: [mdx(), sitemap()],
 });

ce6e830c05804039d13fd9adec3220605f6ea35f
mbsocial <mbsocial.site@outlook.com>
2025-04-30 21:47:17 -0300

    Update site URL and base path in Astro configuration for MBSocial

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://mbsocial.github.io',
+	base: '/mbsocial/',
 	integrations: [mdx(), sitemap()],
 });
